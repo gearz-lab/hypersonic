2b76d20974e752e0877b6ac2cdaaa1d4401808d6
andrerpena <andrerpena@gmail.com>
2015-07-11 09:20:33 -0300

    Introducing the CollectionHelper

---
+++ test/CollectionHelperSpec.js
@@ -0,0 +1,28 @@
+import Chai from 'Chai';
+import metadataEvaluator from '../src/lib/helpers/collectionHelper.js';
+const assert = Chai.assert;
+
+describe('CollectionHelper', function() {
+
+    describe('toObject', function () {
+        it('Basic usage', function () {
+            let myArray = [{id: "a", val: 55}, {id: "b", val: 1}];
+            let object = metadataEvaluator.toObject(myArray, 'id');
+            console.log(object);
+            assert.strictEqual(object.a.id, 'a');
+            assert.strictEqual(object.a.val, 55);
+            assert.strictEqual(object.b.id, 'b');
+            assert.strictEqual(object.b.val, 1);
+        });
+        it('Using arbitrary property as key', function () {
+            let myArray = [{name: "a", val: 55}, {name: "b", val: 1}];
+            let object = metadataEvaluator.toObject(myArray, 'name');
+            console.log(object);
+            assert.strictEqual(object.a.name, 'a');
+            assert.strictEqual(object.a.val, 55);
+            assert.strictEqual(object.b.name, 'b');
+            assert.strictEqual(object.b.val, 1);
+        });
+    });
+
+});
