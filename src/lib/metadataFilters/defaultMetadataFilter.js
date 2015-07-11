606f4fcb9c4ea7f79b51ce74ac733a5f85ed634a
andrerpena <andrerpena@gmail.com>
2015-07-11 12:22:55 -0300

    DefaultMetadataFilter now supports the required metadata

--- src/lib/metadataFilters/defaultMetadataFilter.js
+++ src/lib/metadataFilters/defaultMetadataFilter.js
@@ -2,8 +2,14 @@ import expressionEvaluator from '../expressionEvaluator.js';
 
 class DefaultMetadataFilter {
     filter(metadata, model) {
+        if(!metadata) {
+            throw new Error('metadata is required');
+        }
         if(!model) {
             throw new Error('model is required');
+        }
+        if(metadata.required) {
+            metadata.invalid = {value: true, message: `The field ${metadata.name} is required`};
         }
         return metadata;
     }
