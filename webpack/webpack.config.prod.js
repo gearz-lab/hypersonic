15071127067d776bb29bc45e4483105333669df9
andrerpena <andrerpena@gmail.com>
2015-07-18 10:02:45 -0300

    Changing the prod webpack config

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -25,7 +25,7 @@ export default {
             {test: /\.less$/, loader:  ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")},
             {test: /\.json$/, loader: 'json'},
             {test: /\.jpe?g$|\.gif$|\.png$/, loader: 'file?name=[name].[ext]'},
-            {test: /\.eot$|\.ttf$|\.svg$|\.woff2?$/, loader: 'file?name=[name].[ext]'}
+            {test: /\.eot|\.ttf|\.svg|\.woff2?/, loader: 'file?name=[name].[ext]'}
         ]
     },
 
