5cb6a8bb0c234f0e3f24b5dd06f32de19b580947
andrerpena <andrerpena@gmail.com>
2015-07-18 10:11:33 -0300

    FIxing the gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var run = require('gulp-run');
 
-gulp.task('dev', function () {
+gulp.task('default', function () {
     // building the docs require React compilation, which is made automatically
     // by Babel, so it makes more sense to just run a node script.
     // Running it through Gulp it not easy.
