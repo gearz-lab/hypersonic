c3029da5c2d22045a7f3988015ed67d96fd344ba
andrerpena <andrerpena@gmail.com>
2015-07-18 10:17:44 -0300

    Fixing gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
 var gulp = require('gulp');
 var run = require('gulp-run');
 
-gulp.task('default', function () {
-    // building the docs require React compilation, which is made automatically
-    // by Babel, so it makes more sense to just run a node script.
-    // Running it through Gulp it not easy.
-    return run('webpack').exec();
+gulp.task('build', function () {
+    return run('webpack --config webpack.config.prod.js').exec();
 });
+
+gulp.task('default', ['build']);
