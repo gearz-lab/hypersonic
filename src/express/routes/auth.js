b565c202e629bc1a28b0a72e9f7d4bd127c2dbb0
Andre Pena <andrerpena@gmail.com>
2015-07-25 00:56:17 -0300

    Writing basic auth cookie when Google returns

--- src/express/routes/auth.js
+++ src/express/routes/auth.js
@@ -3,8 +3,22 @@ var passport = require('passport');
 
 var router = express.Router();
 
+// /auth/authenticate
+router.route('/authenticate').get(function(req, res) {
+  var authCookie = {};
+  authCookie.isAuthenticated = req.user != undefined;
+  if(authCookie.isAuthenticated) {
+    authCookie.user = {
+      name: req.user.displayName,
+      image: req.user._json.image.url
+    }
+    res.cookie('gearz.auth', JSON.stringify(authCookie), { maxAge: 9000000, httpOnly: false });
+  }
+  res.redirect('/');
+});
+
 router.route('/google/callback').get(passport.authenticate('google', {
-    successRedirect: '/users/',
+    successRedirect: '/auth/authenticate',
     failure: '/error'
 }));
 
--- src/express/routes/default.js
+++ src/express/routes/default.js
@@ -4,7 +4,7 @@ var passport = require('passport');
 var router = express.Router();
 
 router.route('**').get(function(req, res){
-    res.render('layout.jsx', { user: req.user });
+    res.render('layout.jsx');
 });
 
 module.exports = router;
