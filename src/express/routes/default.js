f8b9fb7b23b024d9eac1df017fd48ba9a66aa46f
Andre Pena <andrerpena@gmail.com>
2015-07-24 23:59:55 -0300

    Improving the default route

--- src/express/routes/default.js
+++ src/express/routes/default.js
@@ -1,3 +1,10 @@
-module.exports = function(req, res){
-    res.render('layout.jsx');
-};
+var express = require('express');
+var passport = require('passport');
+
+var router = express.Router();
+
+router.route('**').get(function(req, res){
+    res.render('layout.jsx', { user: req.user });
+});
+
+module.exports = router;
