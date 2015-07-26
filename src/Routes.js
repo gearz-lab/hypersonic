be60e761b070504e1356f9f4be900e9220acb6b4
Andre Pena <andrerpena@gmail.com>
2015-07-26 18:45:19 -0300

    Upgrading react-router Wireframe for the login page Refactored client routes to support the login

--- src/Routes.js
+++ src/Routes.js
@@ -4,14 +4,18 @@ import App from './views/App.js';
 import HomePage from './views/Home.js';
 import About from './views/About.js';
 import LiveSchemaEditor from './views/LiveSchema.js';
+import Login from './views/Login.js';
 
-import {Route, DefaultRoute, NotFoundRoute} from 'react-router';
+import {Route, Routes, DefaultRoute, NotFoundRoute} from 'react-router';
 
 export default (
-    <Route name='app' path='/' handler={App}>
-        <DefaultRoute handler={HomePage}/>
-        <Route name='home' path='index' handler={HomePage} />
-        <Route name='about' path='about' handler={About} />
-        <Route name='liveSchemaEditor' path='liveSchemaEditor' handler={LiveSchemaEditor}  />
+    <Route>
+        <Route name='app' path='/' handler={App}>
+            <DefaultRoute handler={HomePage}/>
+            <Route name='home' path='index' handler={HomePage} />
+            <Route name='about' path='about' handler={About} />
+            <Route name='liveSchemaEditor' path='liveSchemaEditor' handler={LiveSchemaEditor}  />
+        </Route>
+        <Route name='login' path='/login' handler={Login} />
     </Route>
 )
---
+++ src/views/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var Login = React.createClass({
+    render: function() {
+        return (
+            <div>
+                This is the login page
+            </div>
+        );
+    }
+});
+
+module.exports = Login;
