e1b88c45e76a9bc51d21cc76d91791ab40fa1ba7
Andre Pena <andrerpena@gmail.com>
2015-08-09 23:01:10 -0300

    Eliminating the * route for the API

--- src/express/routes/api.js
+++ src/express/routes/api.js
@@ -8,9 +8,4 @@ var router = express.Router();
 usersApi.setup(router);
 mainMenuApi.setup(router);
 
-router.route('*').get(function(req, res) {
-    //res.contentType('application/json');
-    res.send('API is wortking');
-});
-
 module.exports = router;
