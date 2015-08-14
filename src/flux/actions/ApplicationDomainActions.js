953e9198feed229ff72470ed2a89b5c607cb23e0
Andre Pena <andrerpena@gmail.com>
2015-08-13 22:03:16 -0300

    ApplicationDomainActions.js and clientApi.js

---
+++ src/flux/actions/ApplicationDomainActions.js
@@ -0,0 +1,23 @@
+import Actions from './Actions.js';
+import AppDispatcher from '../dispatchers/AppDispatcher.js';
+import ApplicationDomainConstants from './ApplicationDomainConstants.js';
+import clientApi from '../api/clientApi.js';
+
+class ApplicationDomainActions extends Actions {
+
+    /**
+     * Loads the current logged user
+     */
+    loadMainMenu() {
+
+        this.load({
+            loadAction: ApplicationDomainConstants.LOAD_APPLICATION_DOMAIN,
+            failAction: ApplicationDomainConstants.LOAD_APPLICATION_DOMAIN_FAILED,
+            successAction: ApplicationDomainConstants.LOAD_APPLICATION_DOMAIN_SUCCESS,
+            clientApiFunction: clientApi.applicationDomain.load
+        });
+
+    }
+}
+
+export default new ApplicationDomainActions();
--- src/flux/api/clientApi.js
+++ src/flux/api/clientApi.js
@@ -1,10 +1,12 @@
 import users from './LoggedUserClientApi.js';
 import mainMenu from './MainMenuClientApi.js';
+import applicationDomain from './ApplicationDomainClientApi.js';
 
 class ClientApi{
     constructor() {
         this.users = users;
         this.mainMenu = mainMenu;
+        this.applicationDomain = applicationDomain;
     }
 }
 
