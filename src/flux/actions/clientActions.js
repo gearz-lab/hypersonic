f9559ab9773a4f8f9b4e0bc1154173f2ec9c1bfd
Andre Pena <andrerpena@gmail.com>
2015-08-23 02:36:25 -0300

    Yes, I can't make Flux to work well with the current object state from views... Rolling back

--- src/flux/actions/clientActions.js
+++ src/flux/actions/clientActions.js
@@ -1,14 +1,12 @@
 import LoggedUserActions from './LoggedUserActions.js';
 import MainMenuActions from './MainMenuActions.js';
 import ApplicationDomainActions from './ApplicationDomainActions.js';
-import CurrentEntityActions from './CurrentEntityActions.js';
 import RouterActions from './RouterActions.js';
 
 let clientActions = {
     loggedUser: LoggedUserActions,
     mainMenu: MainMenuActions,
     applicationDomain: ApplicationDomainActions,
-    currentEntity: CurrentEntityActions,
     router: RouterActions
 };
 
--- src/flux/stores/clientStores.js
+++ src/flux/stores/clientStores.js
@@ -1,14 +1,12 @@
 import LoggedUserStore from './LoggedUserStore.js';
 import MainMenuStore from './MainMenuStore.js';
 import ApplicationDomainStore from './ApplicationDomainStore.js';
-import CurrentEntityStore from './CurrentEntityStore.js';
 import RouterStore from './RouterStore.js';
 
 let clientStores = {
     loggedUser: LoggedUserStore,
     mainMenu: MainMenuStore,
     applicationDomain: ApplicationDomainStore,
-    currentEntity: CurrentEntityStore,
     router: RouterStore
 };
 
