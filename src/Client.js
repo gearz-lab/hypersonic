f3c2510135d04e2ccc0ccbaf8658dd66e9394faf
Andre Pena <andrerpena@gmail.com>
2015-08-24 20:41:41 -0300

    Finishing moving styles from Client.js to styles.less

--- src/Client.js
+++ src/Client.js
@@ -1,14 +1,11 @@
+import React from 'react';
+
 // router
 import routerActions from './flux/actions/RouterActions.js';
 
 // styles
-
-import datePicker from 'react-widgets/dist/css/react-widgets.css';
-import bootstrapSocial from 'bootstrap-social/bootstrap-social.css';
-
 import styles from './less/styles.less';
 
-import React from 'react';
 import Router from './Router.js';
 
 // favicon
