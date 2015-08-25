db21c5ab234d82258c136fd99016881f1bb9f50b
Andre Pena <andrerpena@gmail.com>
2015-08-24 21:32:05 -0300

    Script for building the docs

--- docs/Build.js
+++ docs/Build.js
@@ -1,6 +1,10 @@
+import React from 'react';
+import Router from 'react-router';
 import path from 'path';
 import rimraf from 'rimraf-promise';
 import fsep from 'fs-extra-promise';
+
+import Routes from './Routes.js';
 
 const repoRoot = path.resolve(__dirname, '../');
 const docsBuilt = path.join(repoRoot, 'docs-built');
@@ -11,4 +15,13 @@ let pages = ['home.html', 'liveSchemaEditor.html'];
 
 rimraf(docsBuilt)
     .then(() => fsep.mkdir(docsBuilt))
+    .then(() => {
+        let writes = pages.map(fileName => new Promise((resolve, reject) => {
+            Router.run(Routes, '/' + fileName, Handler => {
+               let RootHtml = React.renderToString(React.createElement(Handler));
+                return fsep.writeFile(path.join(docsBuilt, fileName), RootHtml)
+                    .then(write => resolve(write));
+            });
+        }));
+    })
     .then(() => console.log('Built docs'));
