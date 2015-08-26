9b8f02a09d923dc395e548cb2359fb39dc58a0f2
Andre Pena <andrerpena@gmail.com>
2015-08-25 21:40:46 -0300

    Trying to make the ClientLoader.js to work

---
+++ docs/pages/ClientLoader.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Router from 'react-router';
+import Routes from '../Routes.js';
+
+if(window) {
+    window.React = React;
+    window.Router = Router;
+    window.Routes = Routes;
+}
+
+const ClientLoader = React.createClass({
+
+    render: function () {
+
+        let browserInitScriptObj = {
+            __html:
+                `
+                window.Router.run(window.Routes, .window.Router.RefreshLocation, Handler => {
+                  React.render(
+                    window.React.createElement(Handler), document.getElementById('content_place_holder'));
+                });
+            `
+        };
+
+        return (
+            <div>
+                <div id="content_place_holder">
+                </div>
+                <script dangerouslySetInnerHTML={browserInitScriptObj} />
+            </div>
+        );
+    }
+});
+
+export default ClientLoader;
