ae52d264ff2ce11a14e07045e132a705007090ff
André Pena <andrerpena@gmail.com>
2016-03-25 09:07:56 -0300

    Huge clean-up (Migrating to Postgres)

--- src/server/api/usersServerApi.js
+++ src/server/api/usersServerApi.js
@@ -1,30 +1,11 @@
-import UserDal from '../lib/repositories/UserRepository.js';
-let users = new UserDal();
-
 export default {
     setup(router, appConfig, db) {
         if (!router) throw Error('\'router\' should be truthy');
         if (!appConfig) throw Error('\'appConfig\' should be truthy');
         if (!db) throw Error('\'db\' should be truthy');
 
-        // routes
-
         router.route('/users/loggeduser').get(function(req, res) {
             res.send(req.user);
         });
-
-        router.route('/users/:id').get(function(req, res) {
-            let userId = req.params.id;
-            db.connect((error, connection) => {
-                users.find(connection, userId, (error, user) => {
-                    connection.close();
-                    if(error) {
-                        throw error;
-                    }
-                    res.send(user);
-                });
-            });
-        });
-
     }
 }
--- src/server/lib/database/constants.js
+++
@@ -1,7 +0,0 @@
-export default {
-    DB_GEARZ_GLOBAL: 'gearz_global',
-    DB_DEFAULT: 'gearz_tenancy1',
-    TABLE_USERS: 'user',
-    TABLE_ENTITIES: 'entity',
-    USER_ADMIN: 'admin'
-}
