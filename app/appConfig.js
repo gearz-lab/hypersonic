b08640d53d26b95388a72236a8b0fc157b123635
André Pena <andrerpena@gmail.com>
2016-03-25 08:05:04 -0300

    Fixing NPM scripts

--- app/appConfig.js
+++ app/appConfig.js
@@ -1,7 +1,7 @@
 import contact from './entities/contact/contact';
 
 export default {
-    connectionString: 'postgres://postgres:ph4r40h@localhost:5433/CRM',
+    connectionString: 'postgres://postgres:52Ag98d5@localhost:5433/CRM',
     entities: [
         contact
     ]
