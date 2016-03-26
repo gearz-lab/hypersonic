592527ce0bdc8bc0758ee7b0571316dbb244ad64
André Pena <andrerpena@gmail.com>
2016-03-26 14:07:15 -0300

    Update to Babel 6

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,5 @@
 /* eslint no-var: 0 */
-require('./register-babel');
+require("babel-register");
 process.env.NODE_ENV = 'production';
 var config = require('./webpack/webpack.config.prod.js');
 module.exports = config;
