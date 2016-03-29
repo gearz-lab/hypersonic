a7c41407860bf8467cbfdfe1575fdcbb230c07fb
André Pena <andrerpena@gmail.com>
2016-03-29 14:45:35 -0300

    Adding Flow

--- src/client/api/EntityClientApi.js
+++ src/client/api/EntityClientApi.js
@@ -1,3 +1,4 @@
+/* @flow */
 import http from 'axios';
 
 export default {
@@ -6,7 +7,7 @@ export default {
      * @param entityName
      * @param entityId
      */
-    load: function(entityName, entityId) {
+    load: function (entityName:string, entityId:number) {
         if (!entityName) throw Error('\'entityName\' should be truthy');
         if (!entityId) throw Error('\'entityId\' should be truthy');
 
@@ -18,9 +19,10 @@ export default {
      * @param entityName The name of the entity being saved
      * @param entity The entity being saved
      */
-    save: function(entityName, entity) {
+    save: function (entityName:string, entity:any) {
         if (!entityName) throw Error('\'entityName\' should be truthy');
         if (!entity) throw Error('\'entity\' should be truthy');
+
 
         return http.post(`/api/entity/${entityName}/new/`, entity);
     },
@@ -30,7 +32,7 @@ export default {
      * @param entityName
      * @param criteria
      */
-    search: function(entityName, criteria) {
+    search: function (entityName:string, criteria:string) {
         if (!entityName) throw Error('\'entityName\' should be truthy');
         if (criteria === undefined || criteria === null) throw Error('\'criteria\' should not be null or undefined');
 
